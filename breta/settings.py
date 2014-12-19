608ac1236fb2cbe8d4d7c573f59627972b0baf79
Taras Lyapun <taraslyapun@gmail.com>
2014-12-19 10:12:26 +0200

    Added accounts application and User model

--- breta/settings.py
+++ breta/settings.py
@@ -18,7 +18,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$$b*2%yetxq**-*)^70v31ml!^p@#$d(s=hyb4jbdg^u*^p^(3'
+SECRET_KEY = os.environ.get('SECRET_KEY', '$$b*2%yetxq**-*)^70v31ml!^p@#$d(s=hyb4jbdg^u*^p^(3')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,8 +77,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
