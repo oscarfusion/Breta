879da54c227a29e091e371608233c463d83cfd6f
Volodymyr Gamula <v.gamula@gmail.com>
2015-01-18 13:51:29 +0200

    T5 Add extrafield for users search

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -6,4 +6,10 @@ from ..models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email', 'phone', 'city')
+        fields = ('id', 'first_name', 'last_name', 'email', 'phone', 'city', 'is_current_user',)
+        read_only_fields = ('is_current_user',)
+
+    is_current_user = serializers.SerializerMethodField()
+
+    def get_is_current_user(self, obj):
+        return getattr(obj, 'is_current_user', None)
