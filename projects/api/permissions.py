9064fcec7b39146ee96ea849b4df94c998b00974
Volodymyr Gamula <v.gamula@gmail.com>
2015-03-02 00:49:51 +0200

    T105 Only project owners can create projects

--- projects/api/permissions.py
+++ projects/api/permissions.py
@@ -3,6 +3,8 @@ from rest_framework import permissions
 
 class ProjectPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
+        if request.method == 'POST':
+            return request.user.is_authenticated() and not request.user.developer.all()
         return request.user.is_authenticated()
 
     def has_object_permission(self, request, view, obj):
