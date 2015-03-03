5f60b678a8b68721a7dce07b757b6bb0b5e3ef96
Taras Lyapun <taraslyapun@gmail.com>
2015-03-03 20:05:29 +0200

    Lazy initialization for mailchimp

--- accounts/mailchimp_api.py
+++ accounts/mailchimp_api.py
@@ -3,10 +3,8 @@ import mailchimp
 from django.conf import settings
 
 
-mailchimp_api = mailchimp.Mailchimp(settings.MAILCHIMP_API_KEY)
-
-
 def subscribe_user(user):
+    mailchimp_api = mailchimp.Mailchimp(settings.MAILCHIMP_API_KEY)
     mailchimp_api.lists.subscribe(settings.MAILCHIMP_DEFAULT_LIST, {
         'email': user.email
     }, merge_vars={
