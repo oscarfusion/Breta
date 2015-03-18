5145501375bfdfea528fa4bda90199c59370505d
Taras Lyapun <taraslyapun@gmail.com>
2015-03-18 15:28:17 +0200

    added pipeline to installed apps

--- breta/settings.py
+++ breta/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'rest_framework.authtoken',
     'django_jenkins',
-    # 'pipeline',
+    'pipeline',
     'widget_tweaks',
     'corsheaders',
     'constance',
