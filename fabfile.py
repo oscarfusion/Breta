698f387f92429d00dd85ec15438830b71c605021
Volodymyr Gamula <v.gamula@gmail.com>
2015-03-19 10:32:36 +0200

    added excludes

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@ from fabric.api import env, run, prefix
 from fabric.contrib.project import rsync_project
 
 env.hosts = ['root@104.131.243.227']
-RSYNC_EXCLUDES = ['local_settings.py', '.git', '*.pyc']
+RSYNC_EXCLUDES = ['local_settings.py', '.git', '*.pyc', 'htmlconv', 'mediafiles']
 
 
 def rsync():
