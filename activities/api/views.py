0fb6736b98cdaef053692eaec963ab170054fa1b
Volodymyr Gamula <v.gamula@gmail.com>
2015-04-01 17:24:53 +0300

    T162 Filtering activities by date

--- activities/api/views.py
+++ activities/api/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import viewsets
 
 from .serializers import ActivitySerializer
@@ -13,5 +14,11 @@ class ActivityViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         qs = self.queryset.order_by('-created_at')
         if 'project' in self.request.QUERY_PARAMS:
-            return qs.filter(project=self.request.QUERY_PARAMS['project'])
+            qs = qs.filter(project=self.request.QUERY_PARAMS['project'])
+        if 'type' in self.request.QUERY_PARAMS:
+            act_type = self.request.QUERY_PARAMS['type']
+            if act_type == 'last-week':
+                qs = qs.filter(created_at__gte=timezone.now()-timezone.timedelta(days=7))
+            elif act_type == 'last-month':
+                qs = qs.filter(created_at__gte=timezone.now()-timezone.timedelta(days=31))
         return qs
