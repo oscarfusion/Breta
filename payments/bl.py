e3040431f1701d95235368ddada92eee9e520200
Volodymyr Gamula <v.gamula@gmail.com>
2015-09-22 23:42:35 +0300

    Fixed payout transactions

--- payments/bl.py
+++ payments/bl.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 
 from django.core.exceptions import PermissionDenied
-from django.db.models import Sum
+from django.db.models import Sum, Q
 from constance import config
 
 from accounts.models import User
@@ -10,17 +10,16 @@ from . import stripe_api
 from . import email
 from .exceptions import PaymentException
 from .models import Transaction, CreditCard, PayoutMethod
+from .utils import get_transactions_summ
 
 
 def get_user_balance(user_id):
-    received_costs = Transaction.objects.filter(receiver__id=user_id).aggregate(Sum('amount'), Sum('fee'))
-    escrow_amount = received_costs.get('amount__sum') or Decimal(0)
-    withdrawal_fee = received_costs.get('fee__sum') or Decimal(0)
-    referral_amount = Transaction.objects.filter(referrer__id=user_id).aggregate(Sum('referrer_amount')).get('referrer_amount__sum') or Decimal(0)
-    paid_sum = Transaction.objects.filter(payer__id=user_id).aggregate(Sum('amount'), Sum('fee'))
-    milestones_amount = paid_sum.get('amount__sum') or Decimal(0)
-    fee_amount = paid_sum.get('fee__sum') or Decimal(0)
-    return escrow_amount - withdrawal_fee + referral_amount - milestones_amount - fee_amount
+    all_transactions = Transaction.objects.filter(
+        Q(receiver__id=user_id) |
+        Q(payer__id=user_id) |
+        Q(referrer__id=user_id)
+    ).all()
+    return get_transactions_summ(user_id, all_transactions)
 
 
 def get_fee_and_referrer_amount(amount):
@@ -110,12 +109,15 @@ def create_payout_transaction(user, amount):
     payout_method = PayoutMethod.objects.filter(user=user, is_active=True).first()
     user_escrow_amount = get_user_balance(user.id)
     if payout_method and user_escrow_amount >= amount:
-        transfer = stripe_api.create_transfer(amount, payout_method.stripe_recipient_id, 'Withdraw transfer')
+        developer_fee = (amount * Decimal(str(config.DEVELOPER_FEE / 100.0))) or Decimal(0)
+        amount_to_transfer = amount - developer_fee
+        transfer = stripe_api.create_transfer(amount_to_transfer, payout_method.stripe_recipient_id, 'Withdraw transfer')
         instance = Transaction.objects.create(
             payout_method=payout_method,
             stripe_id=transfer.id,
             extra_data=transfer.to_dict(),
-            amount=amount,
+            amount=amount_to_transfer,
+            fee=developer_fee,
             transaction_type=Transaction.PAYOUT,
             payer=user
         )
--- payments/utils.py
+++ payments/utils.py
@@ -1,6 +1,8 @@
-from functools import partial
 import datetime
+from decimal import Decimal
+from functools import partial
 import pytz
+
 from django.utils import timezone
 
 
@@ -21,9 +23,13 @@ def transaction_display_at(date):
 
 
 def filter_transaction(user, transaction):
+    from .models import Transaction
     now = timezone.now()
     if user == transaction.payer:
-        return True
+        if transaction.transaction_type == Transaction.ESCROW:
+            return True
+        elif transaction.transaction_type in [Transaction.PAYOUT, Transaction.PAYPAL_PAYOUT]:
+            return transaction.displayed_at <= now
     elif user == transaction.receiver:
         return transaction.displayed_at <= now
     return False
@@ -32,3 +38,28 @@ def filter_transaction(user, transaction):
 def get_visible_transactions(user, transactions):
     _filter_transaction = partial(filter_transaction, user)
     return filter(_filter_transaction, transactions)
+
+
+def transaction_amount_for_summ(user_id, now, transaction):
+    from .models import Transaction
+    if user_id == transaction.payer_id:
+        if transaction.transaction_type in [
+            Transaction.MILESTONE,
+            Transaction.PAYOUT,
+            Transaction.PAYPAL_PAYOUT
+        ]:
+            return -transaction.amount - (transaction.fee or Decimal(0))
+    elif user_id == transaction.receiver_id:
+        if transaction.transaction_type == Transaction.ESCROW:
+            return transaction.amount
+        if (
+            transaction.transaction_type == Transaction.MILESTONE
+            and transaction.displayed_at <= now
+        ):
+            return transaction.amount
+    return Decimal(0)
+
+
+def get_transactions_summ(user_id, transactions):
+    _amount = partial(transaction_amount_for_summ, user_id, timezone.now())
+    return sum(map(_amount, transactions))
